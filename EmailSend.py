77d8720c9e22ccd4f674e78c9182737052e1f208
phhuy06 <pqh0612@gmail.com>
2023-09-22 11:19:04 +0700

    final_commit

--- EmailSend.py
+++ EmailSend.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 excel_file = os.getenv("excel_path")
@@ -20,20 +22,40 @@ for index, row in df.iterrows():
     recipient_name.append(recipient_name_inexcel)
     recipient_SBD.append(recipient_SBD_inexcel)
 
-subject = os.getenv("header") #header email
+
+html = """\
+<html>
+  <head></head>
+  <body>
+    <p style="color: red;">
+        decor được anh ạ
+    </p>
+  </body>
+</html>
+"""
+
+subject = "header" #header email
 body = os.getenv("body") #body email
 sender = os.getenv("sender") #sender email
 password = os.getenv("password") #application password
 
-def send_email(subject, body, sender, recipient_email, password):
-    msg = MIMEText(body)
+cnt = 0
+
+
+def send_email(subject, body, sender, recipient_email, password, cnt):
+    msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = ', '.join(recipient_email)
+    msg['To'] = recipient_email
+    part2 = MIMEText(html, 'html')
+    msg.attach(part2)
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
        smtp_server.login(sender, password)
        smtp_server.sendmail(sender, recipient_email, msg.as_string())
-    print("Message sent!")
+    print("Message sent " + str(cnt))
 
+for recipient in recipient_email:
+    cnt = cnt + 1
+    send_email(subject, body, sender, recipient, password, cnt)
+print("Done!!")
 
-send_email(subject, body, sender, recipient_email, password)
