be30bd8282a604af3242029695111d429edaa18e
phhuy06 <134865180+phhuy06@users.noreply.github.com>
2024-09-09 21:33:34 +0700

    Update EmailSend.py

--- EmailSend.py
+++ EmailSend.py
@@ -7,7 +7,7 @@ import os
 
 load_dotenv()
 
-excel_file = os.getenv("excel_path")
+excel_file = "./test.xlsx"
 df = pd.read_excel(excel_file)
 
 recipient_email = [] #list email 
@@ -19,30 +19,21 @@ for index, row in df.iterrows():
     recipient_name_inexcel = row["Name"]
     recipient_SBD_inexcel = row["SBD"]
     recipient_email.append(recipient_email_inexcel)
-    recipient_name.append(recipient_name_inexcel)
-    recipient_SBD.append(recipient_SBD_inexcel)
-
+    recipient_name.append(recipient_name_inexcel) #to show name in email
+    recipient_SBD.append(recipient_SBD_inexcel) #to show SBD in email
 
 html = """\
 <html>
   <head></head>
   <body>
-    <p style="color: red;">
+    <p>
         Body
     </p>
   </body>
 </html>
 """
 
-subject = "header" #header email
-body = "body" #body email
-sender = os.getenv("sender") #sender email
-password = os.getenv("password") #application password
-
-cnt = 0
-
-
-def send_email(subject, body, sender, recipient_email, password, cnt):
+def send_email(subject, sender, recipient_email, password, cnt):
     msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
     msg['From'] = sender
@@ -54,8 +45,14 @@ def send_email(subject, body, sender, recipient_email, password, cnt):
        smtp_server.sendmail(sender, recipient_email, msg.as_string())
     print("Message sent " + str(cnt))
 
+subject = "subject" #subject
+sender = os.getenv("sender") #sender email
+password = os.getenv("password") #email application password
+
+cnt = 0
+
 for recipient in recipient_email:
     cnt = cnt + 1
-    send_email(subject, body, sender, recipient, password, cnt)
+    send_email(subject, sender, recipient, password, cnt)
 print("Done!!")
 
